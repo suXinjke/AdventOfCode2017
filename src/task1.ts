150c41c53d4895fce39aef21a26e972fec38b3b7
suXin <suxinjke@gmail.com>
2017-12-09 14:46:11 +0700

    finished task 1

--- src/task1.ts
+++ src/task1.ts
@@ -1,1 +1,19 @@
-console.log( `do whatever ${"now"}` )
+
+let input = process.argv[2].split( '' ).map( digit => Number( digit ) )
+
+const step = process.argv[3] !== undefined ? input.length / 2 : 1
+
+input = input.concat( input.slice( 0, step ) )
+
+let sum = 0
+
+for ( let i = 0 ; i < input.length - 1 ; i++ ) {
+    const digit1 = input[i]
+    const digit2 = input[i+step]
+
+    if ( digit1 === digit2 ) {
+        sum += digit1
+    }
+}
+
+console.log( sum )
