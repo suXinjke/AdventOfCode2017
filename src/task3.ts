20bb2884359395f7982cf9d25a9a9730109a01f6
suXin <suxinjke@gmail.com>
2017-12-10 12:57:53 +0700

    finish task 3

--- src/task3.ts
+++ src/task3.ts
@@ -1,42 +1,114 @@
+export {}
 const input = Number( process.argv[2] );
+const DOING_PART_1 = process.argv[3] === undefined
 
-let level = 0;
-let multiplier = 1;
-let maxLevelNumber = 1;
-
-while ( maxLevelNumber < input ) {
-    multiplier += 2
-    maxLevelNumber = multiplier * multiplier
-    level++
+enum Direction {
+    Right,
+    Up,
+    Left,
+    Down
 }
 
-let mark = ( multiplier - 2 ) * ( multiplier - 2 ) + 1 + ( level - 1 )
+class Field {
+    points: {
+        [index: string]: number
+    } = {}
 
-let x = level;
-let y = 0;
+    x: number = 0
+    y: number = 0
 
-// move up
-for ( let j = 0 ; j < level && mark !== input ; j++ ) {
-    y++;
-    mark++;
+    steps: number = 1
+
+    move( direction: Direction ): number {
+        this.x += 
+            direction === Direction.Right ? 1 :
+            direction === Direction.Left ? -1 :
+            0
+
+        this.y += 
+            direction === Direction.Up ? 1 :
+            direction === Direction.Down ? -1 :
+            0
+
+        this.steps++
+        return this.steps
+    }
+
+    makeCoords( x: number, y: number ) {
+        return `${x},${y}`
+    }
+
+    getPoint( x: number, y: number ) {
+        return this.points[ this.makeCoords( x, y ) ] || 0
+    }
+
+    setPoint( value: number ) {
+        this.points[ this.makeCoords( this.x, this.y ) ] = value
+    }
+
+    getMaxNumberForLevel( level: number ) {
+        return Math.pow( 1 + ( 2 * level ), 2 )
+    }
+
+    getSumOfAdjastentSquares(): number {
+        const { x, y } = this
+        return (
+            this.getPoint( x - 1, y + 1 ) + this.getPoint( x, y + 1 ) + this.getPoint( x + 1, y + 1 ) +
+            this.getPoint( x - 1, y     ) + this.getPoint( x, y     ) + this.getPoint( x + 1, y     ) +
+            this.getPoint( x - 1, y - 1 ) + this.getPoint( x, y - 1 ) + this.getPoint( x + 1, y - 1 )
+        )
+    }
 }
 
-// move left
-for ( let j = 0 ; j < multiplier - 1 && mark !== input ; j++ ) {
-    x--;
-    mark++;
+
+function main() {
+    const field = new Field()
+    field.setPoint( 1 )
+    
+    let level = 0
+    let maxLevelNumber = field.getMaxNumberForLevel( level )
+
+    // Walk around field drawing a spiral and write the required number each step
+    while ( maxLevelNumber < input ) {
+        
+        level++
+        maxLevelNumber = field.getMaxNumberForLevel( level )
+
+        const movementSeries: { didntHitTheWall: () => boolean, direction: Direction }[] = [
+            { direction: Direction.Right,   didntHitTheWall: () => field.x < level },
+            { direction: Direction.Up,      didntHitTheWall: () => field.y < level },
+            { direction: Direction.Left,    didntHitTheWall: () => field.x > -level },
+            { direction: Direction.Down,    didntHitTheWall: () => field.y > -level },
+            { direction: Direction.Right,   didntHitTheWall: () => field.x < level }
+        ]
+
+        for ( let i = 0 ; i < movementSeries.length ; i++ ) {
+            const movement = movementSeries[i]
+            while ( movement.didntHitTheWall() ) {
+                
+                const steps = field.move( movement.direction )
+
+                if ( DOING_PART_1 ) {
+                    field.setPoint( steps )
+
+                    if ( steps === input ) {
+                        const manhattanDistance = Math.abs( -field.x ) + Math.abs( -field.y )
+                        console.log( `Manhattan Distance for step ${steps} at ${field.x} ${field.y} is ${manhattanDistance}` )
+                        return
+                    }
+                } else {
+                    const sum = field.getSumOfAdjastentSquares()
+
+                    field.setPoint( sum )
+                    const result = field.getPoint( field.x, field.y )
+                    if ( result > input ) {
+                        console.log( `Answer is ${result} on step ${steps} at ${field.x} ${field.y}` )
+                        return
+                    }
+                }
+            }
+        }
+    }
 }
 
-// move down
-for ( let j = 0 ; j < multiplier - 1 && mark !== input ; j++ ) {
-    y--;
-    mark++;
-}
-
-// move right
-for ( let j = 0 ; j < multiplier - 1 && mark !== input ; j++ ) {
-    x++;
-    mark++;
-}
-
-console.log( Math.abs( -x ) + Math.abs( -y ) )
+main()
